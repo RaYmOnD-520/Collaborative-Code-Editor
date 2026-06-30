8ef6a6b12a498818fcff84f5f0616515083f8164
Raymond <wonghuaiwen@gmail.com>
2026-06-30 23:57:12 +1000

    feat: add ConnectionManager class for room tracking

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,29 @@
 from fastapi import FastAPI, WebSocket
 
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_rooms: dict[str, list[WebSocket]] = {}
+
+    async def connect(self, websocket: WebSocket, room_id: str):
+        await websocket.accept()
+        if room_id not in self.active_rooms:
+            self.active_rooms[room_id] = []
+        self.active_rooms[room_id].append(websocket)
+
+    def disconnect(self, websocket: WebSocket, room_id: str):
+        if room_id in self.active_rooms:
+            self.active_rooms[room_id].remove(websocket)
+            if len(self.active_rooms[room_id]) == 0:
+                del self.active_rooms[room_id]
+
+    async def broadcast(self, message: str, room_id: str, sender: WebSocket):
+        if room_id in self.active_rooms:
+            for websocket in self.active_rooms[room_id]:
+                if websocket != sender:
+                    await websocket.send_text(message)
+
+
 app = FastAPI()
 
 @app.get("/")
